13404cdfccddb8ca418643c7d56e66e6782d58e1
PrantaChakraborty <cbpranta@gmail.com>
2020-10-04 12:20:38 +0600

    final commit

--- weather/urls.py
+++ weather/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.view),
+    path('', views.view, name='home'),
 ]
--- weather/views.py
+++ weather/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 import requests
 from .models import City
+from .forms import CityForm
+# import json
 from django.http import HttpResponse
+
 
 # Create your views here.
 
@@ -9,16 +12,41 @@ from django.http import HttpResponse
 def view(request):
     req = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=0f57ba0243ae78a736aa8ad8372551fb'
     cities = City.objects.all()
+    if request.method == "POST":
+        form = CityForm(request.POST)
+        form.save()
+    form = CityForm()
     weather_data = []
     for city in cities:
-        city_weather = requests.get(req.format(city)).json()
-        weather = {
-            'city': city,
-            'temperature': city_weather['main']['temp'],
-            'description': city_weather['weather'][0]['description'],
-            'icon': city_weather['weather'][0]['icon']
-        }
-        weather_data.append(weather)
+        try:
+            city_weather = requests.get(req.format(city)).json()
+            weather = {
+                'city': city,
+                'temperature': city_weather['main']['temp'],
+                'description': city_weather['weather'][0]['description'],
+                'icon': city_weather['weather'][0]['icon']
+            }
+            weather_data.append(weather)
+        except:
+            return HttpResponse('Bad request')
+    return render(request, 'weather/index.html', {'weather_data': weather_data, 'form': form})
 
-    return render(request, 'weather/index.html', {'weather_data': weather_data})
 
+'''
+def view_from_form(request):
+    weather_data = []
+    if request.method == "POST":
+        city_name = str(request.POST.get('city'))
+        jsonList = []
+        req = requests.get(
+            f"http://api.openweathermap.org/data/2.5/weather?q={city_name}&units=imperial&appid=0f57ba0243ae78a736aa8ad8372551fb")
+        jsonList.append(json.loads(req.content))
+        userData = {}
+        for data in jsonList:
+            userData['city'] = city_name
+            userData['temperature'] = data['main']['temp']
+            userData['description'] = data['weather'][0]['description']
+            userData['icon'] = data['weather'][0]['icon']
+        weather_data.append(userData)
+    return render(request, 'weather/index2.html', {'weather_data': weather_data})
+'''
